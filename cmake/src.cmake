c5ec4e967a6105cc0c71ecf64307bf7a1f048ae3
Joan Massich <sik@visor.udg.edu>
2018-03-07 19:05:31 +0100

    Generate matio .h.in
    Remove deprecated property
    
    Add variables
    
    add some types + version to config

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -1,3 +1,15 @@
+set(MATIO_PLATFORM ${CMAKE_SYSTEM_PROCESSOR}-unknown-${CMAKE_SYSTEM_NAME})
+
+configure_file(
+  "${PROJECT_SOURCE_DIR}/cmake/matio_pubconf.cmake.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/matio/src/matio_pubconf.h"
+  ESCAPE_QUOTES @ONLY)
+
+configure_file(
+  "${PROJECT_SOURCE_DIR}/cmake/matioConfig.cmake.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/matio/src/matioConfig.h"
+  ESCAPE_QUOTES @ONLY)
+
 set(src_SOURCES
   ${PROJECT_SOURCE_DIR}/matio/src/endian.c
   ${PROJECT_SOURCE_DIR}/matio/src/mat.c
@@ -11,11 +23,11 @@ set(src_SOURCES
   ${PROJECT_SOURCE_DIR}/matio/src/snprintf.c
   ${PROJECT_SOURCE_DIR}/matio/src/read_data.c
   ${PROJECT_SOURCE_DIR}/matio/src/mat5.h
-  ${PROJECT_SOURCE_DIR}/matio/src/matio_pubconf.h.in
   ${PROJECT_SOURCE_DIR}/matio/src/mat73.h
   ${PROJECT_SOURCE_DIR}/matio/src/matio_private.h
   ${PROJECT_SOURCE_DIR}/matio/src/mat4.h
-  ${PROJECT_SOURCE_DIR}/matio/src/matioConfig.h.in
   ${PROJECT_SOURCE_DIR}/matio/src/matio.h
+  ${CMAKE_CURRENT_BINARY_DIR}/matio/src/matio_pubconf.h
+  ${CMAKE_CURRENT_BINARY_DIR}/matio/src/matioConfig.h
 )
 add_library(matio STATIC ${src_SOURCES} )

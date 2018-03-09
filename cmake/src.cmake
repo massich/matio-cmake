91a19b4ed8b40c5048414b6ff6fcf5f243aa36c6
Joan Massich <mailsik@gmail.com>
2018-03-09 18:56:46 +0100

    Add build to CIs (#1)
    * Add build to CIs
    
    * [rm] include only src
    
    * Generate matio .h.in
    
    Remove deprecated property
    
    Add variables
    
    add some types + version to config
    
    * Add Configuration variables to drive XXX.cmake.in
    
    add more config vars
    
    HAVE_STDARG_H
    
    * add target_include_directories
    
    * add generate of export header
    
    * more
    
    * more
    
    * use modern cmake
    
    * fix windows getopt and threads
    
    fix windows getopt and threads
    
    * set HAVE_XXXX_H flag to 1 splicitly

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -1,3 +1,15 @@
+set(MATIO_PLATFORM ${CMAKE_SYSTEM_PROCESSOR}-unknown-${CMAKE_SYSTEM_NAME})
+
+configure_file(
+  "${PROJECT_SOURCE_DIR}/cmake/matio_pubconf.cmake.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/matio/src/matio_pubconf.h"
+  ESCAPE_QUOTES @ONLY)
+
+configure_file(
+  "${PROJECT_SOURCE_DIR}/cmake/matioConfig.cmake.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/matio/src/matioConfig.h"
+  ESCAPE_QUOTES @ONLY)
+
 set(src_SOURCES
   ${PROJECT_SOURCE_DIR}/matio/src/endian.c
   ${PROJECT_SOURCE_DIR}/matio/src/mat.c
@@ -11,11 +23,43 @@ set(src_SOURCES
   ${PROJECT_SOURCE_DIR}/matio/src/snprintf.c
   ${PROJECT_SOURCE_DIR}/matio/src/read_data.c
   ${PROJECT_SOURCE_DIR}/matio/src/mat5.h
-  ${PROJECT_SOURCE_DIR}/matio/src/matio_pubconf.h.in
   ${PROJECT_SOURCE_DIR}/matio/src/mat73.h
   ${PROJECT_SOURCE_DIR}/matio/src/matio_private.h
   ${PROJECT_SOURCE_DIR}/matio/src/mat4.h
-  ${PROJECT_SOURCE_DIR}/matio/src/matioConfig.h.in
   ${PROJECT_SOURCE_DIR}/matio/src/matio.h
+  ${CMAKE_CURRENT_BINARY_DIR}/matio/src/matio_pubconf.h
+  ${CMAKE_CURRENT_BINARY_DIR}/matio/src/matioConfig.h
 )
 add_library(matio STATIC ${src_SOURCES} )
+target_include_directories(matio
+    PRIVATE ${PROJECT_SOURCE_DIR}/matio/src/
+    PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/matio/src/
+)
+
+if(NOT WIN32)
+  target_link_libraries(matio PUBLIC m)
+else()
+  target_link_libraries(matio PUBLIC ${GETOPT_LIB})
+endif()
+
+if(HAVE_HDF5)
+  target_link_libraries(matio
+      PUBLIC ${HDF5_LIBRARIES}
+  )
+endif()
+
+if(HAVE_ZLIB)
+  target_link_libraries(matio
+      PUBLIC ZLIB::ZLIB
+  )
+endif()
+
+# XXX not sure it's the right thing to do...
+set_target_properties(matio PROPERTIES
+  CXX_STANDARD_REQUIRED ON
+  CXX_VISIBILITY_PRESET hidden
+  VISIBILITY_INLINES_HIDDEN 1)
+
+# This generates matio_export.h
+include(GenerateExportHeader)
+generate_export_header(matio)

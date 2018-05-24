7f9b0b4d2ff7e0cdada4eb7a20353a18e25d4fed
Joan Massich <mailsik@gmail.com>
2018-05-24 17:15:10 +0200

    Ship libmatio as an Appveyor's artifact (#6)

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -35,10 +35,12 @@ target_include_directories(matio
     PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/matio/src/
 )
 
+
 if(NOT WIN32)
   target_link_libraries(matio PUBLIC m)
 else()
   target_link_libraries(matio PUBLIC ${GETOPT_LIB})
+  set_target_properties(matio PROPERTIES OUTPUT_NAME libmatio)
 endif()
 
 if(HDF5_FOUND)
@@ -58,6 +60,18 @@ set_target_properties(matio PROPERTIES
   CXX_VISIBILITY_PRESET hidden
   VISIBILITY_INLINES_HIDDEN 1)
 
+
 # This generates matio_export.h
 include(GenerateExportHeader)
 generate_export_header(matio)
+
+set_target_properties(matio PROPERTIES PUBLIC_HEADER "${PROJECT_SOURCE_DIR}/matio/src/matio.h;${CMAKE_CURRENT_BINARY_DIR}/matio/src/matio_pubconf.h;${CMAKE_CURRENT_BINARY_DIR}/matio_export.h") # XXX: check whether matio_pubconf.h or matioConfig.h is the current strategy (one of the two is deprected)
+
+
+# 'make install' to the correct locations (provided by GNUInstallDirs).
+install(TARGETS matio EXPORT libmatio
+        PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
+# install(EXPORT libmatio NAMESPACE matio:: DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake)

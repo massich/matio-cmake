d41b2b3b8c488e923dbc256cd8c13ce45ca370b0
Johan Mabille <johan.mabille@gmail.com>
2018-05-31 16:57:23 +0200

    shared library build with cmake (#12)

--- cmake/thirdParties.cmake
+++ cmake/thirdParties.cmake
@@ -5,6 +5,14 @@ find_package(HDF5)
 
 find_package(ZLIB)
 
+if (HDF5_FOUND)
+	set(HAVE_HDF5 1)
+endif()
+
+if(ZLIB_FOUND)
+	set(HAVE_ZLIB 1)
+endif()
+
 # FindHDF5.cmake does not expose a modern CMake Target
 
 if (HDF5_FOUND AND NOT TARGET HDF5::HDF5)

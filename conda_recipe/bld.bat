c19a14ca00eeadb9b4dabb128ecb0b0fbb7cb2d2
Johan Mabille <johan.mabille@gmail.com>
2018-05-31 11:30:10 +0200

    cmake packaging fix for conda (#9)
    by removing the regular installation using CPack

--- conda_recipe/bld.bat
+++ conda_recipe/bld.bat
@@ -3,10 +3,15 @@ cd build
 
 set CMAKE_CONFIG="Release"
 
-cmake -LAH -G "Visual Studio 14 2015 Win64" ^
-  -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%         ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%      ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%   ^
-  ..
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+	..
+	
+if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target install
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1

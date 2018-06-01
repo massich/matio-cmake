755be484a624be07be9cdfa568b352724d79f08f
Joan Massich <sik@visor.udg.edu>
2018-06-01 18:45:55 +0200

    export config as matio.cmake

--- cmake/src.cmake
+++ cmake/src.cmake
@@ -93,9 +93,9 @@ set(public_headers
 set_target_properties(matio PROPERTIES PUBLIC_HEADER "${public_headers}")
 
 # 'make install' to the correct locations (provided by GNUInstallDirs).
-install(TARGETS matio matio-static EXPORT libmatio
+install(TARGETS matio matio-static EXPORT matio
         PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
         ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
-install(EXPORT libmatio NAMESPACE matio:: DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake)
+install(EXPORT matio NAMESPACE MATIO:: DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake)
